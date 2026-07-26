28b073b18bfb7fdb6fd409191c4f3a7ad3fdbbef
abnishsinghb <58207171+abnishsinghb@users.noreply.github.com>
2026-07-26 15:09:06 +0530

    Create Constants class for configuration file locations
    Added a Constants class for framework-wide constants including file locations and an in-memory locator map.

---
+++ src/test/java/util/Constants.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.util.Map;
+
+/**
+ * Framework-wide constants: file locations for configuration and the
+   * object-repository, plus the in-memory locator map populated by
+   * ConfigReader.initializeJsonFile() at suite start-up.
+   */
+public class Constants {
+
+	public static Map<String, Object> map = null;
+
+	public static final String PROPERTIES_FILE_LOCATION =
+  			System.getProperty("user.dir") + "\\config.properties";
+
+	public static final String OBJECTREPOSITORY_FILE_LOCATION =
+  			System.getProperty("user.dir") + "\\ObjectRepository.json";
+
+	public static final String INPUJSON_FILE_LOCATION =
+  			System.getProperty("user.dir") + "\\test-inputJsonFiles\\";
+}
