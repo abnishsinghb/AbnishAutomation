44c0c4c2688ae552cd969dc0c1a6460a7e085203
abnishsinghb <58207171+abnishsinghb@users.noreply.github.com>
2026-07-26 17:51:06 +0530

    Refactor file path separators for cross-platform compatibility

--- src/test/java/util/Constants.java
+++ src/test/java/util/Constants.java
@@ -1,22 +1,23 @@
 package util;
 
+import java.io.File;
 import java.util.Map;
 
 /**
  * Framework-wide constants: file locations for configuration and the
-   * object-repository, plus the in-memory locator map populated by
-   * ConfigReader.initializeJsonFile() at suite start-up.
-   */
+	 * object-repository, plus the in-memory locator map populated by
+	 * ConfigReader.initializeJsonFile() at suite start-up.
+	 */
 public class Constants {
 
-	public static Map<String, Object> map = null;
+    public static Map<String, Object> map = null;
 
-	public static final String PROPERTIES_FILE_LOCATION =
-  			System.getProperty("user.dir") + "\\config.properties";
+    public static final String PROPERTIES_FILE_LOCATION =
+	            System.getProperty("user.dir") + File.separator + "config.properties";
 
-	public static final String OBJECTREPOSITORY_FILE_LOCATION =
-  			System.getProperty("user.dir") + "\\ObjectRepository.json";
+    public static final String OBJECTREPOSITORY_FILE_LOCATION =
+	            System.getProperty("user.dir") + File.separator + "ObjectRepository.json";
 
-	public static final String INPUJSON_FILE_LOCATION =
-  			System.getProperty("user.dir") + "\\test-inputJsonFiles\\";
+    public static final String INPUJSON_FILE_LOCATION =
+	            System.getProperty("user.dir") + File.separator + "test-inputJsonFiles" + File.separator;
 }
